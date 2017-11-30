1bf90202dc3c25e814f0151cc7a83f3ef591871a
Kelseys1993 <31227275+Kelseys1993@users.noreply.github.com>
2017-12-01 10:04:08 +1300

    js added

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,19 @@
 //custom js
-  $(document).ready(function(){
+(function(){ //iife starts ****
+
+$(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
   });
-
-(function(){ //iife starts ****
-
-
 
 var count = 0;
 var countEl = document.getElementById("count");
 var add = document.getElementById('plus');
 var sub = document.getElementById('minus');
+var next = document.getElementById('firstBtn');
+var prev1 = document.getElementById('prevOne');
+prev1.addEventListener('click', prevDiv, false);
+next.addEventListener('click', nextDiv, false);
 sub.addEventListener('click', minus, false);
 add.addEventListener('click', plus, false);
 
@@ -27,7 +29,22 @@ function minus(){
     countEl.value = count;
   }  
 }
-
+function nextDiv(){
+  if($('input:radio:checked').length > 0){
+$('#formOne').fadeOut(800,function(){
+  $('#formTwo').fadeIn(800);
+});
+}else{
+ 
+}
+}
+function prevDiv(){
+  $('#formTwo').fadeOut(800,function(){
+  $('#formOne').fadeIn(800);
+});
+}
+console.dir(next)
+console.log($('#demo-form').val);
 
 
 })()//iife ends ******
