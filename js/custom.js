214711a0942fa089c9208b2dffe2f0dcb71a0994
Kelseys1993 <31227275+Kelseys1993@users.noreply.github.com>
2017-12-04 22:28:24 +1300

    new function

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,7 @@ var sub = document.getElementById('minus');
 var btn = document.getElementsByClassName('btn-large');
 var close = document.getElementById('modal-button2');
 var home = document.getElementById('logo');
+ var radioValues = [];
 init();
 
 function init(){ 
@@ -24,7 +25,8 @@ btn[5].addEventListener('click', prevDiv3, false);
 btn[3].addEventListener('click', nextDiv3, false);
 
 btn[3].addEventListener('click', getFormData, false);
-
+btn[0].addEventListener('click',  getRadioValue, false );
+btn[1].addEventListener('click',  getRadioValue2, false );
 sub.addEventListener('click', minus, false);
 add.addEventListener('click', plus, false);
 close.addEventListener('click', startAgain, false);
@@ -51,6 +53,7 @@ document.getElementById('formTwo').style.display = 'block';
 function prevDiv(){
 document.getElementById('formTwo').style.display = 'none';
 document.getElementById('formOne').style.display = 'block';
+
 
 }
 function nextDiv2(){
@@ -84,7 +87,72 @@ function startAgain(){
 }
 console.log(dataArray);
 
-
+     function getRadioValue() {
+       
+            var radios = theForm.elements['group1'];
+            var rdValue; 
+            for (var i=0; i<radios.length; i++) {
+                var someRadio = radios[i];
+                if (someRadio.checked) {
+                    rdValue = someRadio.value;
+                    break;
+                }
+                else rdValue = 'noRadioChecked';
+            }
+            if (rdValue == 'Auckland') {
+              radioValues.push(rdValue);
+              console.log(radioValues)
+            } else if( rdValue == 'Wellington'){
+               radioValues.push(rdValue);
+              console.log(radioValues)
+            } else if ( rdValue == 'New Plymouth'){
+               radioValues.push(rdValue);
+            console.log(radioValues)
+            } else if ( rdValue == 'Napier'){
+               radioValues.push(rdValue);
+            console.log(radioValues)
+            } else if(rdValue == 'noRadioChecked'){
+                alert('Start location needed to proceed');
+            } else {
+              console.log('break');
+            }
+        }
+   function getRadioValue2() {
+       
+            var radios = theForm.elements['group2'];
+            var rdValue; 
+            for (var i=0; i<radios.length; i++) {
+                var someRadio = radios[i];
+                if (someRadio.checked) {
+                    rdValue = someRadio.value;
+                    break;
+                }
+                else rdValue = 'noRadioChecked';
+            }
+            if (rdValue == 'waitomo') {
+              radioValues.push(rdValue);
+              console.log(radioValues)
+            } else if( rdValue == 'taupo'){
+               radioValues.push(rdValue);
+              console.log(radioValues)
+            } else if ( rdValue == 'rotorua'){
+               radioValues.push(rdValue);
+            console.log(radioValues)
+            } else if ( rdValue == 'matamata'){
+               radioValues.push(rdValue);
+            console.log(radioValues)
+            } else if ( rdValue == 'tauranga'){
+               radioValues.push(rdValue);
+            console.log(radioValues)
+            }else if ( rdValue == 'castlepoint'){
+               radioValues.push(rdValue);
+            console.log(radioValues)
+            } else if(rdValue == 'noRadioChecked'){
+                alert('Start location needed to proceed');
+            } else {
+              console.log('break');
+            }
+        }
 
 
   //This is click event for the form
@@ -113,6 +181,5 @@ console.log(dataArray);
 
   console.log(values);
   }
-
 
 })()//iife ends ******
